e7924ab0b1552f6fce167abbc1455d4dc9478c7e
YaoShengHan114514 <yghmjgn24r@privaterelay.appleid.com>
2026-09-07 22:11:04 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -3,8 +3,8 @@ import type { ProfileConfig } from "../types/config";
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "まつざか ゆき",
-	bio: "世界は大きい、君は行かなければならない",
+	name: "我很莜陌",
+	bio: "If liking you is wrong, I don't want to be right. Ever!",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
